bbdc33d2602430936330172971c9b923664737e6
Emin YTM <eminyetim25@gmail.com>
2023-02-07 15:40:12 +0300

    firstt

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace BarcodeGeneratorReader
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Barekod");
+        }
+    }
+}
